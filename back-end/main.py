35941665fe009f7d6211ff3629e13a80e37728f0
Dark Tornado <dt3141592@naver.com>
2023-10-03 01:32:13 +0900

    request test

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import requests, time
 
 app = FastAPI()
 
 @app.get("/")
-def test():
-    return "fastapi test"
+def init():
+    ts = round(time.time() * 1000)
+    url = 'https://zaisen.tid-keisei.jp/kr/data/traffic_info.json?ts=' + str(ts)
+    response = requests.get(url)
+    return response.json()
+init()
